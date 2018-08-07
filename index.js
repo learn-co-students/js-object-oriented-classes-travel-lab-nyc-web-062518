96ef8b67369dfd629650983c39ceaf8c7809a4dc
Daniel Ran <danielr1735@gmail.com>
2018-08-07 15:33:44 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
+class Driver {
+  constructor(name, startDate) {
+    this.name = name
+    this.startDate = new Date(startDate)
+  }
 
+  yearsExperienceFromBeginningOf(year) {
+    let dateFromYear = new Date(year, 1, 1)
+    // debugger
+    let diff = dateFromYear - this.startDate
+    let toYears = Math.floor(diff / 1000 / 60 / 60 / 24 / 365)
+    return toYears
+  }
+
+// class Route {
+//   constructor(beginningLocation, endingLocation) {
+//     this.beginningLocation = beginningLocation
+//     this.endingLocation = endingLocation
+//   }
+//
+}
